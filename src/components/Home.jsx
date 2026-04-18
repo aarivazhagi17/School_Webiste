daf04460c73b0b66cdd7733e1ad0869ba4fc61f6
aarivazhagi17 <aarivazhagi17@gmail.com>
2026-04-18 14:31:19 +0530

    new one

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import School6 from '../image/School6.jpg'
 import { useNavigate } from "react-router-dom";
 import aos from 'aos'
 import 'aos/dist/aos.css'
+import School1 from '../video/School1.mp4'
 
 function Home() {
   
@@ -22,6 +23,11 @@ function Home() {
           <img data-aos="fade-down" className='school-image' src={School6} alt="School6"/> <br /> <br />
           <h1 data-aos="fade-up">Welcome to Star Kids</h1> <br />
           <h5 data-aos="fade-up">Join us and watch your child grow!</h5> <br />
+
+          <video  width="500px" height="360px" controls autoPlay
+  loop data-aos="fade-up">
+            <source src={School1} type='video/mp4' />
+          </video>
 
           <button data-aos="fade-up"
         className="ViewMore"
